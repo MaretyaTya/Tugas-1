a5a818be38b75d24dc94f1b4f2e60b11cfa4ffa8
effendikiki <raito12cs@gmail.com>
2018-11-26 08:06:22 +0700

    test

--- Sandbox.java
+++ Sandbox.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
     class Sandbox{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[]arr;
+        int[]arr=new int[5];
+        
+        for(int p:arr){
+            System.out.print("input : ");
+            arr[p]=sc.nextInt();
+        }
+        for(int p=0; p<5; p++){
+            System.out.print(arr[p]);
+        }
     }
 }
